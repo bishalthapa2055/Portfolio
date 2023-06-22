f69e910853ffe862d196ad91985e2869a3f925a1
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-06-22 16:49:02 +0545

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-
+import Typical from "react-typical"
 
 function App() {
 
 
   return (
     <>
-    
-    <h1>Welcome for portfol</h1>
+
+      <h1>Welcome for portfol</h1>
+      <div>
+        <span>
+          <h1>
+            <Typical
+              loop={Infinity}
+              steps={[
+                "Ethustic developer",
+                1000,
+                "MERN developer",
+                1000,
+                "NOde developer",
+                1000,
+                "Java developer",
+                1000,
+                "AI developer",
+                1000,
+
+              ]}
+            />
+          </h1>
+        </span>
+      </div>
     </>
   )
 }
